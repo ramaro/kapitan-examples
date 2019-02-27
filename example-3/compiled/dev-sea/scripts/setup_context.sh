2f52f5a868b2206f80a555f6ee479162e5e793f0
Alessandro De Maria <alessandro.demaria@gmail.com>
2019-02-27 22:49:56 +0000

    Added katacoda project and compiled

--- example-3/compiled/dev-sea/scripts/setup_context.sh
+++ example-3/compiled/dev-sea/scripts/setup_context.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl config set-context dev-sea --cluster minikube --user minikube --namespace dev-sea
+kubectl config set-context dev-sea --cluster kubernetes --user kubernetes-admin --namespace dev-sea
 kubectl config use-context dev-sea
--- example-3/compiled/dev-sea/tuna.md
+++ example-3/compiled/dev-sea/tuna.md
@@ -4,4 +4,4 @@
 Target *dev-sea* is running:
 
 * 1 replicas of *tuna* running image alledm/tuna:v2.0.0 
-* on cluster minikube
+* on cluster kubernetes
